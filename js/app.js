ef8e90b10413d702a5e276fca1d638a1ab7e1057
faisalnadeem <fasinadeem108@gmail.com>
2025-01-14 19:11:38 +0500

    site

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// file download
+
+document
+  .getElementById("downloadButton")
+  .addEventListener("click", function () {
+    // Create a link element
+    const link = document.createElement("a");
+
+    // Set the file URL (update 'example.txt' with your file URL or path)
+    link.href = "./assets/images/file.txt";
+
+    // Set the file name for download
+    link.download = "file.txt";
+
+    // Trigger the download
+    link.click();
+  });
+// Back to Top
+
+// Get the button
+let mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
